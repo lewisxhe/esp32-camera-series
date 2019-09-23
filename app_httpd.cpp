c567459b56234513740c7eedbf5d411bf343523f
lewis <lewishe@outlook.com>
2019-09-23 10:25:26 +0800

    Add print line feed

--- app_httpd.cpp
+++ app_httpd.cpp
@@ -415,7 +415,7 @@ void startCameraServer()
     };
 
     ra_filter_init(&ra_filter, 20);
-    Serial.printf("Starting web server on port: '%d'", config.server_port);
+    Serial.printf("Starting web server on port: '%d'\n", config.server_port);
     if (httpd_start(&camera_httpd, &config) == ESP_OK) {
         httpd_register_uri_handler(camera_httpd, &index_uri);
         httpd_register_uri_handler(camera_httpd, &cmd_uri);
@@ -425,7 +425,7 @@ void startCameraServer()
 
     config.server_port += 1;
     config.ctrl_port += 1;
-    Serial.printf("Starting stream server on port: '%d'", config.server_port);
+    Serial.printf("Starting stream server on port: '%d'\n", config.server_port);
     if (httpd_start(&stream_httpd, &config) == ESP_OK) {
         httpd_register_uri_handler(stream_httpd, &stream_uri);
         httpd_register_uri_handler(stream_httpd, &hmi_uri);
